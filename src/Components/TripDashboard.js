b15a9d97a4ed947cab447bc2c941b86efa24abf6
starstormfall <photon_dazzles_02@icloud.com>
2022-09-18 16:58:03 +0800

    edited routes and added appshell for overall theme

--- src/Components/TripDashboard.js
+++ src/Components/TripDashboard.js
@@ -1,36 +1,5 @@
 import React from "react";
-import {
-  Link,
-  useParams,
-  useNavigate,
-  NavLink,
-  Outlet,
-} from "react-router-dom";
 
 export default function TripDashboard() {
-  // react routes
-  const navigate = useNavigate();
-  const params = useParams();
-
-  return (
-    <div>
-      <p>Trip {params.tripId} Dashboard</p>
-      <div
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-          display: "flex",
-          justifyContent: "space-around",
-          fontSize: "16px",
-        }}
-      >
-        <Link to="calendar">Calendar | </Link>
-        <Link to="wishlist">Wishlist | </Link>
-        <Link to="packinglist">Packing List |</Link>
-        <a href="">Add friend</a>
-      </div>
-      <button onClick={() => navigate("/home")}>Back to Home</button>
-      <Outlet />
-    </div>
-  );
+  return <div>Trip Dashboard</div>;
 }
