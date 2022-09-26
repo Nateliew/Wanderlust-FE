0781a6788c28e863d4881986362f82dde1392efa
root <root@LAPTOP-C6553LAC.localdomain>
2022-09-26 21:21:00 +0800

    monday merge

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 import moment from "moment";
 import React, { useState, useEffect } from "react";
 
-import CountryImage from "./CountryImages";
+// import CountryImage from "./CountryImages";
 
 export default function Single({ trip, handleDelete }) {
   const [source, setSource] = useState();
@@ -33,34 +33,36 @@ export default function Single({ trip, handleDelete }) {
   };
 
   return (
-    <Card shadow="sm" p="xl">
-      <Card.Section>
-        <Image
-          src={countryImages[trip.country] || countryImages.default}
-          height={160}
-          alt="Trips"
-        />
-        {/* {<CountryImage country={trip.country} setSource={setSource} />} */}
-      </Card.Section>
+    <div>
+      <Card shadow="sm" p="xl">
+        <Card.Section>
+          <Image
+            src={countryImages[trip.country] || countryImages.default}
+            height={160}
+            alt="Trips"
+          />
+          {/* {<CountryImage country={trip.country} setSource={setSource} />} */}
+        </Card.Section>
 
-      <Text weight={500} size="lg" mt="md">
-        {trip.country}{" "}
-      </Text>
+        <Text weight={500} size="lg" mt="md" style={{ textDecoration: "none" }}>
+          {trip.country}{" "}
+        </Text>
 
-      <Text mt="xs" color="dimmed" size="sm">
-        {moment(trip.startDate).format("MMM Do YY")} to{" "}
-        {moment(trip.endDate).format("MMM Do YY")}
-      </Text>
-      <Button
-        variant="light"
-        color="blue"
-        fullWidth
-        mt="md"
-        radius="md"
-        onClick={() => handleDelete(trip.id)}
-      >
-        Delete{" "}
-      </Button>
-    </Card>
+        <Text mt="xs" color="dimmed" size="sm">
+          {moment(trip.startDate).format("MMM Do YY")} to{" "}
+          {moment(trip.endDate).format("MMM Do YY")}
+        </Text>
+        <Button
+          variant="light"
+          color="blue"
+          fullWidth
+          mt="md"
+          radius="md"
+          onClick={() => handleDelete(trip.id)}
+        >
+          Delete{" "}
+        </Button>
+      </Card>
+    </div>
   );
 }
